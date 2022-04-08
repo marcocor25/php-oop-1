5398c28c0e33c9ff465de6810138caed1eb880ff
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-04-08 16:29:49 +0200

    Comincio a sviluppare le card dei film

--- class.php
+++ class.php
@@ -1,14 +1,15 @@
 <?php
-
 class Movie {
 
+    public $cover;
     public $title;
     public $description;
     public $genre;
     public $original_language;
 
-    function __construct($_title, $_description, $_genre, $_original_language)
+    function __construct($_cover, $_title, $_description, $_genre, $_original_language)
     {
+        $this->cover = $_cover;
         $this->title = $_title;
         $this->description = $_description;
         $this->genre = $_genre;
--- index.php
+++ index.php
@@ -3,27 +3,28 @@
 require_once __DIR__ . "/class.php";
 
 $ted = new Movie(
+    "https://images.everyeye.it/img-cover/ted-16299-320x450.jpg",
     "Ted",
     "Un orso di peluche festaiolo e sboccato rischia di far fallire i tentativi del suo amico umano di maturare e sposare la fidanzata.",
-    "Commedia",
+    "Comico",
     "🇬🇧",
 );
 
-$bugiardo_bugiardo = new Movie(
-    "Bugiardo Bugiardo",
-    "Un avvocato abituato a vincere le sue cause a suon di bugie si trova irrimediabilmente costretto a dire la verità per 24 interminabili ore.",
-    "Commedia",
+$ghost_movie = new Movie(
+    "https://pad.mymovies.it/filmclub/2012/10/168/locandina.jpg",
+    "Ghost Movie",
+    "Questa parodia dei film horror segue il trasferimento di una giovane coppia in una casa dove si nasconde uno spirito malvagio dalle trovate terribilmente spassose.",
+    "Commedia horror",
     "🇬🇧",
 );
 
 $the_mask = new Movie(
+    "https://m.media-amazon.com/images/M/MV5BOWExYjI5MzktNTRhNi00Nzg2LThkZmQtYWVkYjRlYWI2MDQ4XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg",
     "The Mask",
     "Un mediocre impiegato di banca trova un'antica maschera che scatena i suoi desideri più nascosti, trasformandolo in un burlone spassosissimo.",
     "Commedia",
     "🇬🇧",
 );
-
-var_dump($ted, $bugiardo_bugiardo, $the_mask)
 
 ?>
 
@@ -34,16 +35,35 @@ var_dump($ted, $bugiardo_bugiardo, $the_mask)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movie</title>
+    <link rel="stylesheet" href="./style.css">
+    <title>Movies</title>
 </head>
 
 <body>
 
-    <h1>
-        <?php echo $ted->title ?>
-    </h1>
+    <h1 class="main-title">my movies</h1>
 
+    <div class="container">
 
+        <div class="card">
+            <figure>
+                <img src="<?php echo $ted->cover ?>" alt="">
+            </figure>
+        </div>
+
+        <div class="card">
+            <figure>
+                <img src="<?php echo $ghost_movie->cover ?>" alt="">
+            </figure>
+        </div>
+
+        <div class="card">
+            <figure>
+                <img src="<?php echo $the_mask->cover ?>" alt="">
+            </figure>
+        </div>
+
+    </div>
 
 </body>
 
